b52bc8391672ce4e853fe5d21559da751c37cba6
Zachary Delcoco <zdelcoco@gmail.com>
2023-06-24 23:01:38 -0500

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  displayContent = false;
+  numClicks = 0;
+  clickLog = [0];
+
+  toggleDisplayContent() {
+    this.displayContent = !this.displayContent;
+    this.numClicks++;
+    this.clickLog.push(this.numClicks);
+  };
+
+  getColor() {
+    return this.numClicks >= 5 ? 'blue' : 'transparent';
+  }
+
 }
